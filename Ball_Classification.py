cf1f2d1c9e8b3185f6be68d3b662d8768ae92e08
Ankita Shinde <154900765+theankita@users.noreply.github.com>
2026-02-17 00:17:52 +0530

    Update Ball_Classification.py

--- Ball_Classification.py
+++ Ball_Classification.py
@@ -3,9 +3,8 @@ from sklearn import tree
 #Rough - 1
 #Smooth - 0
 
-#Tennis -1
 #Cricket - 2
-
+#Tennis -1
 def main():
     print("Ball Classification case study")
 
@@ -18,30 +17,28 @@ def main():
     Y = [1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 2, 1, 2, 1, 2]
 
     #Independent variables for training
+
     Xtrain = [[35, 1], [47, 1], [90, 0], [48, 1], [90, 0],[35, 1], [92, 0],[35, 1],[35, 1],[35, 1], [96, 0],[43, 1], [110, 0]]
 
     #Independent variables for testing
+
     Xtest = [[35, 1], [95, 0]]
 
     #Dependent for training
+
     Ytrain = [1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 2, 1, 2]
 
     #Dependent for testing
+
     Ytest = [1, 2]
 
     modelobj = tree.DecisionTreeClassifier()   #Model selection
 
-    trainedmodel = modelobj.fit(Xtrain , Ytrain)  #This model will go on harddisk
+    trainedmodel = modelobj.fit(Xtrain , Ytrain)  #This model go on harddisk
 
-    result = trainedmodel.predict([[35,1]]) #Output - [1 2]
+    result = trainedmodel.predict(Xtest) #Output - [1 2]
 
-    print(type(result))
-
-    if(result == 1):
-        print("Object looks like tennis ball ")
-
-    elif(result == 2):
-        print("Object looks like Cricket Ball")
+    print("Model predicts the object as : ",result)
 
 if __name__ == "__main__":
     main()
